10339e28e6b58185c6eb2f3d984c283be0f2c918
mpadgett <mattpadgett@outlook.com>
2023-10-13 12:23:14 -0500

    initial setup

---
+++ configs/powershell/install.ps1
@@ -0,0 +1,3 @@
+mkdir -f $home\Documents\WindowsPowerShell
+
+cp C:\data\configs\powershell\profile.ps1 $home\Documents\WindowsPowerShell\profile.ps1
---
+++ setup.ps1
@@ -0,0 +1,3 @@
+mkdir -p C:\data\vm_shared
+mkdir -p C:\data\vms
+mkdir -p C:\data\repos
